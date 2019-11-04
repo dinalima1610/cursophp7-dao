ea4d22411354dd3089cfc87c1714b066878dc309
Dina Lima <dinalima.dal@gmail.com>
2019-11-04 09:01:53 -0300

    métodos List

--- index.php
+++ index.php
@@ -5,9 +5,31 @@
 	//$usuarios = $sql->select("SELECT * FROM tb_usuarios");
 	//echo json_encode($usuarios);
 
+	// carrega um usuário
 	$usuario = new Usuario();
 	$usuario->loadById(3);
 	echo $usuario;
 
-	
+	//lista de usuários
+	echo "<br /><br />";
+	$lista = Usuario::getList();
+	echo json_encode($lista);
+
+	echo "<br /><br />";
+
+	//carrega uma lista de usuários buscando pelo login
+	$search = Usuario::search("jo");
+	echo json_encode($search);
+
+	echo "<br /><br />";
+
+	//carrega o usuário usando o login e senha
+	$usuario = new Usuario();
+	$usuario->login("root","!@#$");
+	echo $usuario;
+
+	echo "<br /><br />";
+
+	$usuario->login("root","123");
+	echo $usuario;
 ?>
